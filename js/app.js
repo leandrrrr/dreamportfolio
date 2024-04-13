a4bd8f22c064f50ab8e024419568ae266b101083
léandéré <leandre.mascher.pro@gmail.com>
2024-04-13 21:11:14 +0200

    :lipstick: amelioration du frontend

---
+++ js/app.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const box = document.querySelector('.box');
+
+  document.addEventListener('mousemove', function(e) {
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+
+    box.style.left = mouseX/70 + 'px';
+    box.style.top = mouseY/70 + 'px';
+  });
+});
